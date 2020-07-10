5bdbe41d4f5264bf662317b1abf5765e823b39d2
anikamorris <anikamorris11@gmail.com>
2020-07-09 22:04:52 -0700

    Initial Commit

---
+++ Newsstand/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newsstand
+//
+//  Created by Anika Morris on 7/9/20.
+//  Copyright © 2020 Anika Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
