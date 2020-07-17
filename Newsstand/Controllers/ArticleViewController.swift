d3784a91ca0af529b977f6304bb14c810428123b
anikamorris <anikamorris11@gmail.com>
2020-07-17 00:19:30 -0700

    load web view with article content from url

--- Newsstand/Controllers/ArticleViewController.swift
+++ Newsstand/Controllers/ArticleViewController.swift
@@ -8,12 +8,25 @@
 
 import Foundation
 import UIKit
+import WebKit
 
-class ArticleViewController: UIViewController {
+class ArticleViewController: UIViewController, WKUIDelegate {
     var article: Article!
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = self.article.title
+        guard let url = self.article.url else { return }
+        guard let articleURL = URL(string: url) else { return }
+        let request = URLRequest(url: articleURL)
+        webView.load(request)
     }
 }
